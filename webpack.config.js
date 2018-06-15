0b1ca70f32692cb78057979489305c6b5cebd677
Viktor Ohad <viktorohad@Viktors-MacBook-Pro.local>
2018-06-15 21:21:41 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+const path = require('path');
+const webpack = require('webpack');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const bundleOutputDir = './wwwroot/dist';
+
+module.exports = {
+    context: __dirname,
+    entry: { main: './App/index.ts' },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+                options: {
+                    appendTsSuffixTo: [/\.vue$/]
+                }
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {
+                        scss: ['vue-style-loader', 'css-loader', 'sass-loader'],
+                        sass: ['vue-style-loader', 'css-loader', 'sass-loader?indentedSyntax']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['vue-style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]?[hash]'
+                }
+            }
+        ]
+    },
+    plugins: [new VueLoaderPlugin()],
+    resolve: {
+        alias: {
+            vue$: 'vue/dist/vue.esm.js'
+        },
+        extensions: ['.ts', '.js', '.vue', '.json']
+    },
+    devServer: {
+        historyApiFallback: true,
+        noInfo: true,
+        overlay: true
+    },
+    performance: {
+        hints: false
+    },
+    output: {
+        path: path.join(__dirname, bundleOutputDir),
+        filename: '[name].js',
+        publicPath: 'dist/'
+    },
+    devtool: '#eval-source-map',
+    mode: 'development'
+};
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports.devtool = '#source-map';
+    module.exports.plugins = (module.exports.plugins || []).concat([
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: true,
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true
+        })
+    ]);
+}
